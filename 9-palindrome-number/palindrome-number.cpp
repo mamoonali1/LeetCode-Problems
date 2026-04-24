c86c20400f168e588a93700d38476f10dc26431b
Muhammad Mamoon Ali <144024057+mamoonali1@users.noreply.github.com>
2026-04-24 22:40:45 +0500

    Time: 1 ms (57.74%) | Memory: 8.6 MB (65.18%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers or numbers ending with 0 (except 0)
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+};
